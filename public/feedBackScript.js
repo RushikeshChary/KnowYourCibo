a51dc8deb87699df202cb7bf476eb9cd1601c422
RushikeshChary <ruushid19@gmail.com>
2024-04-14 00:54:58 +0530

    updated

--- public/feedBackScript.js
+++ public/feedBackScript.js
@@ -1,31 +1,43 @@
+// document.getElementById("feedbackForm").onsubmit = async (e) => {
+//   e.preventDefault();
+//   const name = document.getElementById("name").value;
+//   const email = document.getElementById("email").value;
+//   const comments = document.getElementById("comments").value;
+//   const feedbackDate = document.getElementById("feedbackDate").value;
+//   const ratingOptions = document.getElementsByName("rating");
 
+//   let rating;
+//   for (let i = 0; i < ratingOptions.length; i++) {
+//       if (ratingOptions[i].checked) {
+//           rating = ratingOptions[i].value;
+//           break;
+//       }
+//   }
 
+//   // if (!rating) {
+//   //     alert("Please select a rating.");
+//   //     return;
+//   // }
 
-
-document.getElementById("feedbackForm").onsubmit = async (e) => {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const comments = document.getElementById("comments").value;
-    const feedbackDate = document.getElementById("feedbackDate").value;
-    const rating = 3;
-    // const newpassword = document.getElementById("newpassword").value;
-    try {
-      const response = await fetch("/submit-feedback", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name, email, feedbackDate,rating,comments}),
-      });
-      const data = await response.json();
-      if (data.error) {
-        alert(data.error);
-        window.location.href = "/Feedback";
-      } else {
-        alert(data.message);
-        window.location.href = "/Feedback";
-      }
-    } catch (error) {
-      console.error("Error setting password:", error);
-      alert("Error in fetching changes from server");
-    }
-  };
+//   try {
+//       const response = await fetch("/submit-feedback", {
+//           method: "POST",
+//           headers: { "Content-Type": "application/json" },
+//           body: JSON.stringify({ name, email, feedbackDate, rating, comments }),
+//       });
+//       const statusCode = response.statusCode;
+//       const data = await response.json();
+//       if (data.error) {
+//           alert(data.error);
+//           window.location.href = "/Feedback";
+//       } else {
+//           if(statusCode === 400) {
+//             alert(data.message);
+//             window.location.href = "/Feedback";
+//           }
+//       }
+//   } catch (error) {
+//       console.error("Error sending feedback mail", error);
+//       alert("Error in fetching changes from server");
+//   }
+// };

54280b01c0913d2b6a330bf7439b3a6907721f8a
Vijay-602 <135162650+Vijay-602@users.noreply.github.com>
2024-04-13 14:49:47 +0530

    Update feedBackScript.js

---
+++ public/feedBackScript.js
@@ -0,0 +1,27 @@
+document.getElementById("feedbackForm").onsubmit = async (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const comments = document.getElementById("comments").value;
+    const feedbackDate = document.getElementById("feedbackDate").value;
+    const rating = 3;
+    // const newpassword = document.getElementById("newpassword").value;
+    try {
+      const response = await fetch("/submit-feedback", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, feedbackDate,rating,comments}),
+      });
+      const data = await response.json();
+      if (data.error) {
+        alert(data.error);
+        window.location.href = "/Feedback";
+      } else {
+        alert(data.message);
+        window.location.href = "/Feedback";
+      }
+    } catch (error) {
+      console.error("Error setting password:", error);
+      alert("Error in fetching changes from server");
+    }
+  };
